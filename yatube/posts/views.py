158682e6d8a5b22b181f58fbde86bf8c27e9d64a
Данил Кочетов <dmi194@yandex.ru>
2022-11-18 19:29:24 +0300

    Добавлен паджинатор на главную страницу

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 from .models import Group
 POSTS_NUMBER: int = 10  # Количество постов, отображаемых на странице
 
 
 def index(request):
-    posts = Post.objects.select_related('group')[:POSTS_NUMBER]
+    posts = Post.objects.select_related('group').all()
+    paginator = Paginator(posts, POSTS_NUMBER)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
